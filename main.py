ae289e47d1cb2264e1f56e5b0387e1ed2f51cf14
Brabomax <seu-email@gmail.com>
2026-03-25 06:45:47 -0300

    porta corrigida

--- main.py
+++ main.py
@@ -271,5 +271,4 @@ def desligar():
     return jsonify({"s": "ok"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
-
+        app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), threaded=True)
